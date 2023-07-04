1667b8628fc4dea59c2ebed6076fd6b8102f94be
Natalie Nicole <natalienicole@MacBook-Pro.local>
2023-07-04 16:57:01 -0400

    optimized code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const count = 20
 const countNeeded = 10
 // only 10 items are needed, but fetching more in case some items are not
 // images (and will therefore be skipped)
-let arr = []
+const arr = []
 
 const modal = document.querySelector(".modal")
 const imgModal = document.querySelector(".img-modal")
@@ -13,6 +13,7 @@ const descDate = document.querySelector(".desc-date")
 const descText = document.querySelector(".desc-text")
 const closeX = document.querySelector(".close")
 const randomize = document.querySelector(".randomize")
+const imgDivs = document.querySelectorAll(".img")
 
 fetch(`${baseURL}?api_key=${key}&count=${count}`)
   .then(res => res.json())
@@ -41,8 +42,6 @@ fetch(`${baseURL}?api_key=${key}&count=${count}`)
   })
   .catch(err => console.log(err))
 
-console.log(arr)
-
 // ----------------------------------------------------------
 
 randomize.addEventListener("click", e => { location.reload() })
@@ -61,17 +60,12 @@ closeX.addEventListener("mouseout", e => {
   closeX.src = "img/close-x-gray.png"
 })
 
-document.querySelector("#img1").addEventListener("click", loadModal0)
-document.querySelector("#img2").addEventListener("click", loadModal1)
-document.querySelector("#img3").addEventListener("click", loadModal2)
-document.querySelector("#img4").addEventListener("click", loadModal3)
-document.querySelector("#img5").addEventListener("click", loadModal4)
-document.querySelector("#img6").addEventListener("click", loadModal5)
-document.querySelector("#img7").addEventListener("click", loadModal6)
-document.querySelector("#img8").addEventListener("click", loadModal7)
-document.querySelector("#img9").addEventListener("click", loadModal8)
-document.querySelector("#img10").addEventListener("click", loadModal9)
-
+imgDivs.forEach(div => {
+  div.addEventListener("click", e => {
+    setModal(parseInt(div.dataset.id) - 1)
+    toggleModel(e)
+  })
+})
 
 function toggleModel(e) {
   modal.classList.toggle("hidden")
@@ -83,54 +77,4 @@ function setModal(i) {
   descTitle.innerHTML = obj.title
   descDate.innerHTML = obj.date
   descText.innerHTML = obj.explanation
-}
-
-function loadModal0(e) {
-  setModal(0)
-  toggleModel(e)
-}
-
-function loadModal1(e) {
-  setModal(1)
-  toggleModel(e)
-}
-
-function loadModal2(e) {
-  setModal(2)
-  toggleModel(e)
-}
-
-function loadModal3(e) {
-  setModal(3)
-  toggleModel(e)
-}
-
-function loadModal4(e) {
-  setModal(4)
-  toggleModel(e)
-}
-
-function loadModal5(e) {
-  setModal(5)
-  toggleModel(e)
-}
-
-function loadModal6(e) {
-  setModal(6)
-  toggleModel(e)
-}
-
-function loadModal7(e) {
-  setModal(7)
-  toggleModel(e)
-}
-
-function loadModal8(e) {
-  setModal(8)
-  toggleModel(e)
-}
-
-function loadModal9(e) {
-  setModal(9)
-  toggleModel(e)
 }
